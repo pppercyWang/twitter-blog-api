374d09c911835074cc519d0709c976dabf9902d2
wangwensheng <845082868@qq.com>
2019-09-20 16:59:13 +0800

    ss

--- db/information_cache.sql
+++ db/information_cache.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50716
  File Encoding         : 65001
 
- Date: 07/08/2019 10:50:50
+ Date: 20/09/2019 14:59:27
 */
 
 SET NAMES utf8mb4;
@@ -27,26 +27,66 @@ CREATE TABLE `article`  (
   `updated_at` timestamp(0) NULL DEFAULT NULL,
   `deleted_at` timestamp(0) NULL DEFAULT NULL,
   `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `article_type_id` int(10) UNSIGNED NULL DEFAULT 0,
-  `title` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `title` varchar(80) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `likes` int(10) UNSIGNED NULL DEFAULT 0,
   `personal` int(10) UNSIGNED NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `idx_article_deleted_at`(`deleted_at`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 35 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of article
 -- ----------------------------
-INSERT INTO `article` VALUES (1, NULL, NULL, NULL, '# twitter-blog-vue\r\n\r\n因为平时工作用UI组件库用的多。过分依赖组件库，只会导致自己的技术越来越烂。组件的编写，样式的书写，动画，适配，媒体查询等很多都不记得了。\r\n自己一直想着写一个博客。打算在工作之余模仿twitter的一个页面。写一个自己的博客\r\n\r\n### 模仿页面：https://twitter.com/dariel_noel \r\n\r\nrespect for Dariel Noel~~\r\n\r\n### 日志\r\n``` bash\r\n2019/7/25  menu-item组件,hover动画效果\r\n2019/7/26  实现拉幕效果，字体抖动\r\n2019/7/27  实现中部导航栏布局\r\n2019/7/29  中部导航栏\r\n2019/7/31  button组件\r\n2019/8/1   整体布局改造\r\n2019/8/2   个人信息\r\n```\r\n\r\n### 实时进度\r\n![image](https://github.com/pppercyWang/twitter-blog-vue/blob/master/public/img/demo.gif)', 0, 'aaa', NULL, 0, 0);
-INSERT INTO `article` VALUES (2, '2019-08-07 02:28:45', '2019-08-07 02:28:45', NULL, '\r\n``` bash\r\n# install dependencies\r\nnpm install\r\n\r\n\r\n# serve with hot reload at localhost:8081\r\nnpm run dev\r\n\r\n# build for production with minification\r\nnpm run build\r\n\r\n```\r\n# Folder structure\r\n* build - webpack config files\r\n* config - webpack config files\r\n* dist - build\r\n* src -your app\r\n    * api\r\n    * assets\r\n    * lib 封装了axios 和 工具类\r\n    * components - \r\n    * styles\r\n    * views - 视图\r\n    * vuex\r\n    * App.vue\r\n    * main.js - main file\r\n    * routes.js\r\n* static - static assets\r\n\r\n# JWT说明：\r\n``` bash\r\n1. 登录成功后获得userId，将userId存在sessionStorage\r\n2. 在http.js设置了拦截器，每次访问接口都会进入拦截器设置头部信息等\r\n3. token在这里的getToken方法生成。每次访问接口因为时间戳的不同，token也不同\r\n```\r\n', 0, 'README', '', 0, 0);
+INSERT INTO `article` VALUES (23, '2019-08-21 07:59:14', '2019-08-21 07:59:14', NULL, '### Go语言的参数只能是值传递\n值传递是一个拷贝的过程\n\nGo:值传递\n```go\nfunc bbb(list [4]int){\n   for i,_ := range list{\n      list[i] = 0\n   }\n}\nfunc main(){\n   list := [4]int{1,2,3,4}\n   bbb(list)\n   fmt.Println(list )   //[1 2 3 4]\n}\n\n```\n\nJavascript: 引用传递\n```javascript\nlet list = [1,2,3,4]\nfunction  bbb(list){\n    list.forEach((item,index)=>{\n        list[index] = 0\n    })\n}\nbbb(list)\nconsole.log(list)   //[0,0,0,0]\n```\n但是Go语言可以通过指针来实现引用传递的作用\n\n```go\nfunc bbb(p2 *[4]int){    //接受一个[4]int的指针\n   for i,_ := range p2{\n      p2[i] = 0\n   }\n}\nfunc main(){\n   list := [4]int{1,2,3,4}\n   p := &list\n   bbb(p)     //传递指针\n   fmt.Println(list )   //[0 0 0 0]\n}\n```\n既然说go语言的参数传递只能是值传递，所以这里实际上是把指针地址拷贝了一份，而两个地址都指向了main函数的变量list的地址，所以才使list的值发生了改变\n\n![在这里插入图片描述](https://img-blog.csdnimg.cn/20190609203729513.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3dlaXhpbl80MjU2NTEzNw==,size_16,color_FFFFFF,t_70)', 'go语言的值传递', '### Go语言的参数只能是值传递\n值传递是一个拷贝的过程\n\nGo:值传递\n', 0, 0);
+INSERT INTO `article` VALUES (24, '2019-08-21 08:00:53', '2019-08-21 08:00:53', NULL, '@[TOC](简单粗暴console.log出整条原型链)\n```javascript\n//只有函数对象有prototype，但所有对象都有__proto__，所有对象的__proto__指向其构造函数的prototype\nlet a = new Array()\n\nconsole.log(a.__proto__==Array.prototype)  //true  \n\n//第一条线  普通对象之间的继承关系\nconsole.log(Array.prototype.__proto__==Object.prototype)  //true  Array.prototype是一个[]，我们把它看成一个普通对象，\n//它没有prototype（只有函数对象有），但它有__proto__，一个普通对象的__proto__指向Object.prototype\n\nconsole.log(Object.prototype) //{}  而 Object.prototype也是一个普通对象，它没有prototype，只有__proto__\n\nconsole.log(Object.prototype.__proto__) //null  最终指向了null\n\n//第二条线  函数对象之间的继承关系\nconsole.log(Array.prototype.constructor==Array) //true  //prototype是每个构造函数在定义的时候就自动new出的一个原型对象，\n//是一个实例，而这个实例的constructor就指向它的构造函数\n\nconsole.log(Array.prototype) //[]这里又回到了第一条线的第一步\n\nconsole.log(Array.__proto__==Function.prototype) //true  //所有的构造函数的__proto__ 都指向Function.prototype，\n//甚至包括根构造函数Object及Function自身\n\nconsole.log(Function.prototype)  //[Function]  而这时！Function.prototype却不是一个普通对象，而是一个空函数。\n\nconsole.log(Function.prototype.prototype) //undifined 但是它却没有prototype(之前说所有的函数对象都有prototype，它是例外)，但是他有__proto__\n\nconsole.log(Function.prototype.__proto__==Object.prototype) //而它的__proto__又指到了Object.prototype\n\nconsole.log(Object.prototype.__proto__) //null  最终指向了null\n\n```\n总结：\n说到底原型链就是解释JS中对象的继承关系。\n\n* 普通对象之间的继承关系：继承的是方法和属性。最上层是null，最终继承到普通对象本身。而函数的特性只继承到构造函数本身，所以它的实例没有函数特性\n\n* 函数对象之间的继承关系：继承的是函数的特性。最上层是null,最终继承到构造函数本身\n\n这张图画的非常好，借鉴一下\n![在这里插入图片描述](https://img-blog.csdnimg.cn/20190219142940900.jpg?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3dlaXhpbl80MjU2NTEzNw==,size_16,color_FFFFFF,t_70)\n\n*自己对原型链的感悟，如有不对，欢迎指正*', '简单粗暴console.log出整条原型链', '@[TOC](简单粗暴console.log出整条原型链)\n', 0, 0);
+INSERT INTO `article` VALUES (25, '2019-08-21 08:01:37', '2019-08-21 08:01:37', NULL, '关于vuex，我们听到最多的是vuex是一个状态管理容器，可以解决组件之间通信的痛点。但vuex真的只是这样吗？\n\n平时，我更喜欢将vuex比喻成一个**前端程序的数据库**。他可以储存各种数据，需要什么直接过去拿就行。我们都知道，单页面应用只要页面跳转了，data中的数据都会清空。但store中的数据不会清空，只有在页面刷新时，会清空store中的数据\n\n### 一.那我们是不是可以这样设计？\n**当我们需要获取数据时，在action中发起请求，然后将数据直接放到store里面。每当我们进入这个页面，都先判断一下store中有没有这些数据，如果有就直接拿，没有的话就通过action发出请求**\n\n### 二.实际应用\n#### 场景说明：\n这是一个移动端的产品，首页有轮播图组件。每次我们进入这个页面，都需要发起请求获取轮播图的一些数据。大家可以想像一下。通常我们我们使用这个应用的时候，点击美食 > 退回首页 > 点击酒店 >退回首页 ......\n\n我们进入首页的时候非常多，如果每次进入这个页面，都从created钩子去获取数据，会发起很多次请求。其实这是根本没有必要的。利用之前的思想，我们就可以实现如下效果。\n![在这里插入图片描述](https://img-blog.csdnimg.cn/20190709101809911.gif)\n#### Demo地址：\n https://github.com/pppercyWang/vue-typescript-mobile\n**关于分页数据的处理可以看这个PC端的管理平台模板**\nhttps://github.com/pppercyWang/vue-typescript-admin\n### 三.那我所有的请求都要在action中去请求，然后存在store中吗？\n其实是没有必要的。比如说一些列表的数据可以放在store中。但是一些短暂性的请求（比如删除，修改）就直接写在methods中就可以了。\n如果有讲的不好的地方，欢迎指正。qq:845082868\n### 四.总结\n**Vuex大法好！**', 'Vuex在Vue工程的正确使用', '关于vuex，我们听到最多的是vuex是一个状态管理容器，可以解决组件之间通信的痛点。但vuex真的只是这样吗？\n\n平时，我更喜欢将vuex比喻成一个**前端程', 0, 0);
+INSERT INTO `article` VALUES (26, '2019-09-20 03:27:08', '2019-09-20 03:27:08', NULL, 'ssssssss', 'sss', 'ssssssss', 0, 0);
+INSERT INTO `article` VALUES (27, '2019-09-20 06:53:03', '2019-09-20 06:53:03', NULL, '# cccccccccccc ', 'adsdas', '# cccccccccccc ', 0, 0);
+INSERT INTO `article` VALUES (28, '2019-09-20 06:53:09', '2019-09-20 06:53:09', NULL, '# cccccccccccc ', 'vvvv', '# cccccccccccc ', 0, 0);
+INSERT INTO `article` VALUES (29, '2019-09-20 06:53:14', '2019-09-20 06:53:14', NULL, '# cccccccccccc ', 'bbb', '# cccccccccccc ', 0, 0);
+INSERT INTO `article` VALUES (30, '2019-09-20 06:53:29', '2019-09-20 06:53:29', NULL, 'cascsa', 'nnnn', 'cascsa', 0, 0);
+INSERT INTO `article` VALUES (31, '2019-09-20 06:53:35', '2019-09-20 06:53:35', NULL, 'cascsa', 'nnnvvv', 'cascsa', 0, 0);
+INSERT INTO `article` VALUES (32, '2019-09-20 06:53:42', '2019-09-20 06:53:42', NULL, 'cascsa', 'cxzcxz', 'cascsa', 0, 0);
+INSERT INTO `article` VALUES (33, '2019-09-20 06:53:50', '2019-09-20 06:53:50', NULL, 'cascsa', 'vcxvcxsd', 'cascsa', 0, 0);
+INSERT INTO `article` VALUES (34, '2019-09-20 06:53:56', '2019-09-20 06:53:56', NULL, 'cascsa', 'bfgd', 'cascsa', 0, 0);
 
 -- ----------------------------
--- Table structure for article_type
+-- Table structure for article_category
 -- ----------------------------
-DROP TABLE IF EXISTS `article_type`;
-CREATE TABLE `article_type`  (
+DROP TABLE IF EXISTS `article_category`;
+CREATE TABLE `article_category`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp(0) NULL DEFAULT NULL,
+  `updated_at` timestamp(0) NULL DEFAULT NULL,
+  `deleted_at` timestamp(0) NULL DEFAULT NULL,
+  `category_id` int(10) UNSIGNED NOT NULL,
+  `article_id` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_article_category_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 49 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of article_category
+-- ----------------------------
+INSERT INTO `article_category` VALUES (37, '2019-08-21 07:59:14', '2019-08-21 07:59:14', NULL, 13, 23);
+INSERT INTO `article_category` VALUES (38, '2019-08-21 08:00:54', '2019-08-21 08:00:54', NULL, 18, 24);
+INSERT INTO `article_category` VALUES (39, '2019-08-21 08:01:37', '2019-08-21 08:01:37', NULL, 4, 25);
+INSERT INTO `article_category` VALUES (40, '2019-09-20 03:27:09', '2019-09-20 03:27:09', NULL, 6, 26);
+INSERT INTO `article_category` VALUES (41, '2019-09-20 06:53:03', '2019-09-20 06:53:03', NULL, 5, 27);
+INSERT INTO `article_category` VALUES (42, '2019-09-20 06:53:09', '2019-09-20 06:53:09', NULL, 4, 28);
+INSERT INTO `article_category` VALUES (43, '2019-09-20 06:53:14', '2019-09-20 06:53:14', NULL, 13, 29);
+INSERT INTO `article_category` VALUES (44, '2019-09-20 06:53:29', '2019-09-20 06:53:29', NULL, 11, 30);
+INSERT INTO `article_category` VALUES (45, '2019-09-20 06:53:35', '2019-09-20 06:53:35', NULL, 11, 31);
+INSERT INTO `article_category` VALUES (46, '2019-09-20 06:53:42', '2019-09-20 06:53:42', NULL, 8, 32);
+INSERT INTO `article_category` VALUES (47, '2019-09-20 06:53:50', '2019-09-20 06:53:50', NULL, 9, 33);
+INSERT INTO `article_category` VALUES (48, '2019-09-20 06:53:57', '2019-09-20 06:53:57', NULL, 15, 34);
+
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `created_at` timestamp(0) NULL DEFAULT NULL,
   `updated_at` timestamp(0) NULL DEFAULT NULL,
@@ -54,25 +94,27 @@ CREATE TABLE `article_type`  (
   `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `count` int(10) UNSIGNED NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `idx_article_type_deleted_at`(`deleted_at`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  INDEX `idx_category_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for book
+-- Records of category
 -- ----------------------------
-DROP TABLE IF EXISTS `book`;
-CREATE TABLE `book`  (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `created_at` timestamp(0) NULL DEFAULT NULL,
-  `updated_at` timestamp(0) NULL DEFAULT NULL,
-  `deleted_at` timestamp(0) NULL DEFAULT NULL,
-  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `count` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `author` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `type` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `idx_book_deleted_at`(`deleted_at`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+INSERT INTO `category` VALUES (4, '2019-08-19 02:18:49', '2019-08-19 02:18:49', NULL, 'vue', 0);
+INSERT INTO `category` VALUES (5, '2019-08-19 02:19:09', '2019-08-19 02:19:09', NULL, 'typescript', 0);
+INSERT INTO `category` VALUES (6, '2019-08-19 02:19:14', '2019-08-19 02:19:14', NULL, 'nodejs', 0);
+INSERT INTO `category` VALUES (7, '2019-08-19 02:19:21', '2019-08-19 02:19:21', NULL, 'egg.js', 0);
+INSERT INTO `category` VALUES (8, '2019-08-19 02:19:27', '2019-08-19 02:19:27', NULL, 'express', 0);
+INSERT INTO `category` VALUES (9, '2019-08-19 02:19:31', '2019-08-19 02:19:31', NULL, 'vuex', 0);
+INSERT INTO `category` VALUES (10, '2019-08-19 02:19:49', '2019-08-19 02:19:49', NULL, 'twitter-blog-vue', 0);
+INSERT INTO `category` VALUES (11, '2019-08-19 02:20:06', '2019-08-19 02:20:06', NULL, 'linux', 0);
+INSERT INTO `category` VALUES (12, '2019-08-19 02:20:11', '2019-08-19 02:20:11', NULL, 'nginx', 0);
+INSERT INTO `category` VALUES (13, '2019-08-19 02:20:48', '2019-08-19 02:20:48', NULL, 'golang', 0);
+INSERT INTO `category` VALUES (14, '2019-08-19 02:20:52', '2019-08-19 02:20:52', NULL, 'gorm', 0);
+INSERT INTO `category` VALUES (15, '2019-08-19 02:20:56', '2019-08-19 02:20:56', NULL, 'iris', 0);
+INSERT INTO `category` VALUES (16, '2019-08-19 02:21:03', '2019-08-19 02:21:03', NULL, 'element-ui', 0);
+INSERT INTO `category` VALUES (17, '2019-08-19 04:04:08', '2019-08-19 04:04:08', NULL, 'weex', 0);
+INSERT INTO `category` VALUES (18, '2019-08-19 04:04:08', '2019-08-19 04:04:08', NULL, 'javascript', 0);
 
 -- ----------------------------
 -- Table structure for user
@@ -96,39 +138,11 @@ CREATE TABLE `user`  (
   UNIQUE INDEX `username`(`username`) USING BTREE,
   UNIQUE INDEX `mobile`(`mobile`) USING BTREE,
   INDEX `idx_user_deleted_at`(`deleted_at`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for wechat
+-- Records of user
 -- ----------------------------
-DROP TABLE IF EXISTS `wechat`;
-CREATE TABLE `wechat`  (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `created_at` timestamp(0) NULL DEFAULT NULL,
-  `updated_at` timestamp(0) NULL DEFAULT NULL,
-  `deleted_at` timestamp(0) NULL DEFAULT NULL,
-  `customer_id` int(10) UNSIGNED NULL DEFAULT NULL,
-  `wechat_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `wechat_password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `socks_ip` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `wechat_alias` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `wechat_mobile` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `nickname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `sex` int(10) UNSIGNED NULL DEFAULT NULL,
-  `signature` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `country` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `province` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `city` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `small_headimg_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `group_id` int(10) UNSIGNED NULL DEFAULT NULL,
-  `last_login_date` timestamp(0) NULL DEFAULT NULL,
-  `login_device_data` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `status` int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `wechat_id`(`wechat_id`) USING BTREE,
-  INDEX `idx_wechat_deleted_at`(`deleted_at`) USING BTREE,
-  INDEX `idx_wechat_wechat_id`(`wechat_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+INSERT INTO `user` VALUES (1, '2019-08-08 02:47:37', '2019-08-08 02:47:37', NULL, 'percy', 'e10adc3949ba59abbe56e057f20f883e', '', '', '', '', 0, 0, '');
 
 SET FOREIGN_KEY_CHECKS = 1;
