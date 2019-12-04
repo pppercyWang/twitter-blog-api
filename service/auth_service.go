913c3f60a758ccdfac7689ceaef797f861d263d5
pppercywang <845082868@qq.com>
2019-12-04 17:56:48 +0800

    ss

--- service/auth_service.go
+++ service/auth_service.go
@@ -29,13 +29,13 @@ type GitUserStruct struct {
 
 func (u authService) GetUserInfo(m map[string]interface{}) (result models.Result){
 	result.Code = 0
-	retStr :=utils.PostRequest("https://github.com/login/oauth/access_token",m)
+	retStr :=utils.PostRequest("https://github.com/login/oauth/access_token",m)  // 携带code 参数加上 你的 client_id client_secret 去获取 access_token
 	token:= utils.GetUrlParam(retStr,"access_token")
 	if token == "" {
 		result.Code = -1
 		result.Msg = "获取access_token失败"
 	}
-	retStr2 := utils.GetRequest("https://api.github.com/user?access_token=" + token)
+	retStr2 := utils.GetRequest("https://api.github.com/user?access_token=" + token)  // 通过access_token获取用户信息
 	result.Data = retStr2
 	var gitUserStruct GitUserStruct
 	if err := json.Unmarshal([]byte(retStr2), &gitUserStruct); err == nil {
